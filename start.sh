abdbba18c70fee56b5f32cfc67e0fb888751e781
unknown <emilion-1@o2.pl>
2020-09-29 22:24:32 +0200

    zmiana

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 !#/bin/bash
 #podniesienie noda 1
-docker run -d --name="rabbit1" --hostname="rabbit1" -e RABBITMQ_ERLANG_COOKIE="secret string" -e RABBITMQ_NODENAME="rabbit1" --volume=rabbitmq.config:/etc/rabbitmq/rabbitmq.config --volume=definitions.json:/etc/rabbitmq/definitions.json --publish="4369:4369" --publish="5671:5671" --publish="5672:5672" --publish="15671:15671" --publish="15672:15672" --publish="25672:25672" rabbitmq:3-management
+docker run -d --name="rabbit1" --hostname="rabbit1" -e RABBITMQ_ERLANG_COOKIE="secret string" -e RABBITMQ_NODENAME="rabbit1" --volume=rabbitmq.config:/etc/rabbitmq/rabbitmq.config --volume=definitions.json:/etc/rabbitmq/definitions.json --publish="4369:4369" --publish="5671:5671" --publish="5672:5672" --publish="15672:15672" --publish="25672:25672" rabbitmq:3-management
 #odczekanie chwili
 sleep 20
 #uruchomienie noda 2
